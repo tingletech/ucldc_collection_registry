4acf4af6b8b3d6920c42648a5742b87ff5ec9334
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-09-20 16:37:27 -0700

    start of the data model

---
+++ dl_collections/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from dl_collections.models import *
+
+admin.site.register(Collection)
+admin.site.register(Campus)
+admin.site.register(Status)
+admin.site.register(Format)
+admin.site.register(Restriction)
+admin.site.register(Level)
+admin.site.register(Standard)
+admin.site.register(Need)
--- dl_collections/models.py
+++ dl_collections/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Campus(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Status(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Format(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Restriction(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Level(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Standard(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Need(models.Model):
+    name = models.CharField(max_length=255)			#
+
+class Collection(models.Model):
+    name = models.CharField(max_length=255)			#
+    campus = models.ManyToManyField(Campus)	# why not a multi-campus collection?
+    description = models.TextField()
+    url_local = models.CharField(max_length=255)
+    url_oac = models.CharField(max_length=255)
+    url_was = models.CharField(max_length=255)
+    status = models.ManyToManyField(Status)
+    format = models.ManyToManyField(Format)
+    extent = models.BigIntegerField(max_length=255)
+    access_restrictions = models.ManyToManyField(Restriction)
+    metadata_level = models.ManyToManyField(Level)
+    metadata_standard = models.ManyToManyField(Standard)
+    need_for_dams = models.ManyToManyField(Need)
+    ready_for_surfacing = models.BooleanField()
+
