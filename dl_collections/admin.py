da9e9506f610f9af568094274a3336f7c1a53a93
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-09-20 22:16:34 -0700

    show less in admin view

--- dl_collections/admin.py
+++ dl_collections/admin.py
@@ -1,9 +1,4 @@
 from django.contrib import admin
-from dl_collections.models import *
+from dl_collections.models import Collection
 
 admin.site.register(Collection)
-admin.site.register(Campus)
-admin.site.register(Status)
-admin.site.register(Format)
-admin.site.register(Restriction)
-admin.site.register(Need)
