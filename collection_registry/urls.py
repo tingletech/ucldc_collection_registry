4b9a6e7a81cce67990ce27d16847c3ee7b89276d
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2012-09-24 22:41:08 -0700

    reorder regexes

--- collection_registry/urls.py
+++ collection_registry/urls.py
@@ -1,6 +1,6 @@
+# urls.py
 from django.conf.urls import patterns, include, url
 from dl_collections.models import Collection, Campus
-#from dl_collections.sitemap import RegistrySitemap
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,7 +8,6 @@ from ajax_select import urls as ajax_select_urls
 from django.contrib.sitemaps import GenericSitemap
 from django.views.generic.simple import redirect_to
 admin.autodiscover()
-
 
 collection_dict = {
     'queryset': Collection.objects.all(),
@@ -23,23 +22,8 @@ sitemaps = {
     "collection_registry": GenericSitemap(collection_dict),
 }
 
-
 urlpatterns = patterns('',
-    # include the lookup urls
-    #(r'^admin/lookups/', include(ajax_select_urls)),
-
-    # Examples:
-    # url(r'^$', 'collection_registry.views.home', name='home'),
-    #url(r'^$', redirect_to, {'url': '/collection_registry/'}),
-    #url(r'^collection_registry/', include('dl_collections.urls')),
-    url(r'^', include('dl_collections.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}, name='sitemap')
-
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}, name='sitemap'),
+    url(r'^', include('dl_collections.urls')),
 )
