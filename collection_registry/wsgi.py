e0cba4b5cf1b272974e04aae03ef579d0a035f08
Brian Tingle <brian.tingle@ucop.edu>
2012-09-27 00:51:52 -0700

    wsgi needs to tweak python path

--- collection_registry/wsgi.py
+++ collection_registry/wsgi.py
@@ -14,6 +14,11 @@ framework.
 
 """
 import os
+import sys
+from os.path import abspath, dirname, join
+
+sys.path.insert(0, abspath(join(dirname(__file__), "../")))
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "collection_registry.settings")
 
