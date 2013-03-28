5ad1143ba431a6418c50211154b51f91bf460148
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-03-27 18:02:12 -0700

    hook up aobo711/bootstrap-django-admin
    ./bin/pip install https://github.com/aobo711/bootstrap-django-admin/tarball/master

--- collection_registry/settings.py
+++ collection_registry/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'admin_bootstrap/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -123,6 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     # Uncomment the next line to enable the admin:
+    'admin_bootstrap',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
